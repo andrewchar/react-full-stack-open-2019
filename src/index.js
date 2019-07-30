5726c5cc8396491c21f9498d222e58b62690846a
Andrew Charlebois <andrewchar88@gmail.com>
2019-07-30 16:54:12 -0400

    up to step 1.11*

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [positive, setPositive] = useState(0);
+  const [hasVoted, setHasVoted] = useState(false)
+
+  const allVotes = good + neutral + bad;
+
+  useEffect(() => {
+    setPositive(Math.round((good / allVotes) * 100) / 100 + '%');
+  }, [allVotes])
+
+  const vote = (voteOption) => {
+    switch(voteOption) {
+        case 'good':
+            setGood(good + 1);
+            break;
+        case 'neutral':
+            setNeutral(neutral + 1);
+            break;
+        case 'bad':
+            setBad(bad + 1);
+            break;
+    }
+    setHasVoted(true);
+  }
+
+  return (
+    <div>
+        <h2>give feedback</h2>
+        <Button text='good' clickAction={()=>vote('good')} />
+        <Button text='neutral' clickAction={()=>vote('neutral')} />
+        <Button text='bad' clickAction={()=>vote('bad')} />
+        <h2>Statistics</h2>
+        {hasVoted ? (
+            <table>
+                <tbody>
+                    <tr>
+                        <Statistic text='good' stat={good} />
+                    </tr>
+                    <tr>
+                        <Statistic text='neutral' stat={neutral} />
+                    </tr>
+                    <tr>
+                        <Statistic text='bad' stat={bad} />
+                    </tr>
+                    <tr>
+                        <Statistic text='all' stat={allVotes} />
+                    </tr>
+                    <tr>
+                        <Statistic text='positive' stat={positive} />
+                    </tr>
+                </tbody>
+            </table>
+        ) : (
+            <p>no feedback given</p>
+        )}
+
+    </div>
+  )
+}
+
+const Statistic = ({text, stat}) => {
+    return (
+        <>
+            <td>{text}</td>
+            <td>{stat}</td>
+        </>
+    )
+}
+
+const Button = ({clickAction, text}) => <button onClick={clickAction}>{text}</button>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
